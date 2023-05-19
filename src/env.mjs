c55942275b778708fdbea1ebfaf56a511e6e65d5
Piyush <bhangalepiyush@gmail.com>
2023-05-19 22:52:08 +0530

    chore(env(: add hedera config variables

--- src/env.mjs
+++ src/env.mjs
@@ -2,29 +2,20 @@ import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    HEDERA_NETWORK: z.enum(["previewnet", "testnet", "mainnet"]),
+    HEDERA_OPERATOR_ID: z.string().min(1),
+    HEDERA_OPERATOR_KEY: z.string().min(96).max(96),
   },
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
   },
 
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    HEDERA_NETWORK: process.env.HEDERA_NETWORK,
+    HEDERA_OPERATOR_ID: process.env.HEDERA_OPERATOR_ID,
+    HEDERA_OPERATOR_KEY: process.env.HEDERA_OPERATOR_KEY,
   },
 });
