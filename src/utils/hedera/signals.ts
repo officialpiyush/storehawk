d56d808b06941f5185c2b834112926eb072634d1
Piyush <bhangalepiyush@gmail.com>
2023-05-19 22:07:11 +0530

    feat: add initData signal and connection metadata

--- src/utils/hedera/signals.ts
+++ src/utils/hedera/signals.ts
@@ -9,3 +9,12 @@ export const hashConnectAvailableExtension =
 export const hashConnectState = signal<HashConnectConnectionState | null>(null);
 export const hashConnectTopic = signal<string | null>(null);
 export const hashpackExtensionFound = signal<boolean>(false);
+export const initData = signal<HashConnectTypes.InitilizationData | null>(null);
+
+export const StoreHawkHashConnectMetadata: HashConnectTypes.AppMetadata = {
+  name: "StoreHawk",
+  description: "something",
+  // TODO: create and add icon
+  icon: "https://pycz.dev/favicon.svg",
+  url: "http://localhost:3000",
+};
