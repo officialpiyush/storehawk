434d4a7126c8abc31a07e87f66977d15f93c3ffb
Piyush <bhangalepiyush@gmail.com>
2023-05-19 23:19:50 +0530

    chore(eslint): remove @next/next/no-img-element rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "@next/next/no-img-element": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
