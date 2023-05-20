7eb0a03e06f257f148ac70622a17d06a5be39799
Piyush <bhangalepiyush@gmail.com>
2023-05-20 23:12:30 +0530

    feat: loading on data load

--- src/components/listings/ListingSellerCard.tsx
+++ src/components/listings/ListingSellerCard.tsx
@@ -1,4 +1,7 @@
+import { faker } from "@faker-js/faker";
+import { Momentum } from "@uiball/loaders";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 interface ListingSellerCardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -7,8 +10,18 @@ interface ListingSellerCardProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export default function ListingSellerCard(props: ListingSellerCardProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutTime = faker.number.int({ min: 1000, max: 3000 });
+
+    setTimeout(() => {
+      setLoading(false);
+    }, timeoutTime);
+  }, []);
+
   return (
-    <div className="flex w-full flex-col rounded-xl bg-white h-full">
+    <div className="flex h-full w-full flex-col rounded-xl bg-white">
       <div
         className={clsx(
           "flex items-center justify-center gap-2",
@@ -21,10 +34,21 @@ export default function ListingSellerCard(props: ListingSellerCardProps) {
         <div className="font-bold">{props.title}</div>
       </div>
 
-      <div className="flex flex-col items-center justify-center gap-2 overflow-y-auto px-4 py-4">
+      <div className="relative flex flex-col items-center justify-center gap-2 overflow-y-auto px-4 py-4">
         {props.items.map((item) => (
           <div key={item}>{item}</div>
         ))}
+
+        <div
+          className={clsx(
+            "absolute bottom-0 left-0 right-0 top-0",
+            "rounded-xl bg-white",
+            "flex items-center justify-center",
+            !loading && "hidden"
+          )}
+        >
+          <Momentum color="#706a6a" />
+        </div>
       </div>
     </div>
   );
