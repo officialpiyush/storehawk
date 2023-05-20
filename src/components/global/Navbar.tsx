cab4bfca26004c8af19b2345f7d424029c576ca6
Piyush <bhangalepiyush@gmail.com>
2023-05-20 09:43:17 +0530

    feat: complete navbar

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,7 +1,58 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NAVBAR_ITEMS = [
+  {
+    name: "StoreHawk",
+    path: "/",
+    customClassName: "font-lobster font-bold text-lg",
+  },
+  {
+    name: "Orders",
+    path: "/orders",
+  },
+  {
+    name: "Listings",
+    path: "/listings",
+  },
+  {
+    name: "Vendors",
+    path: "/vendors",
+  },
+  {
+    name: "My Ledger",
+    path: "/ledger",
+  },
+  {
+    name: "Tokenized Debt",
+    path: "/tokenized-debt",
+  },
+  {
+    name: "NFT",
+    path: "/nft",
+  },
+];
+
 export default function Navbar() {
-  return <div className="flex items-center justify-between">
-    <div className="bg-[#FF8577] px-6 py-1 rounded-full font-lobster text-lg">
-        StoreHawk
+  const router = useRouter();
+
+  return (
+    <div className="flex items-center justify-between">
+      {NAVBAR_ITEMS.map((item) => (
+        <Link
+          className={clsx(
+            router.pathname === item.path &&
+              "rounded-full bg-[#FF8577] px-6 py-1",
+            "font-medium",
+            item.customClassName && item.customClassName
+          )}
+          href={item.path}
+          key={item.name}
+        >
+          {item.name}
+        </Link>
+      ))}
     </div>
-  </div>;
+  );
 }
