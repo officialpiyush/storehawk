ffdd116c877a9f5caf6921e7b879d69e4ebe369c
Piyush <bhangalepiyush@gmail.com>
2023-05-20 09:10:30 +0530

    feat: add navbar

---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return <div className="flex items-center justify-between">
+    <div className="bg-[#FF8577] px-6 py-1 rounded-full font-lobster text-lg">
+        StoreHawk
+    </div>
+  </div>;
+}
