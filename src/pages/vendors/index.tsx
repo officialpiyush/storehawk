df939478bad76115aecd50ae5037b10c0f8c8074
Piyush <bhangalepiyush@gmail.com>
2023-05-20 13:58:38 +0530

    feat: vendor screen box

---
+++ src/pages/vendors/index.tsx
@@ -0,0 +1,15 @@
+import Chip from "@/components/Chip";
+
+export default function VendorPage() {
+  return (
+    <div className="flex h-full w-full flex-col gap-4">
+      <div className="w-fit">
+        <Chip label="Vendors" className="text-2xl font-bold" />
+      </div>
+
+      <div className="flex-1 h-full w-full bg-[#C0DE77] px-4 py-4 rounded-2xl">
+        .
+      </div>
+    </div>
+  );
+}
