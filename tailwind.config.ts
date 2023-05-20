4e9fbc759cabffd5559154f995c5a6eb09bce8bf
Piyush <bhangalepiyush@gmail.com>
2023-05-20 10:29:40 +0530

    chore: Add tremor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,10 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
