25ac54e2906d2e6597d9101505e2ff32f9a74aac
Piyush <bhangalepiyush@gmail.com>
2023-05-20 09:04:13 +0530

    feat(fonts): add lobster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,14 @@ import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
 
+import { Lobster } from "next/font/google";
+
+const _lobster = Lobster({
+  weight: "400",
+  variable: "--font-lobster",
+  preload: false,
+});
+
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lobster: ["var(--font-lobster)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
