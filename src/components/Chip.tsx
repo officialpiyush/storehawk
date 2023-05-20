ad2d552b4a934a85c9d56ec2c5cb034201c84e0a
Piyush <bhangalepiyush@gmail.com>
2023-05-20 09:43:49 +0530

    feat: add chip component

---
+++ src/components/Chip.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+interface ChipProps extends React.HTMLAttributes<HTMLDivElement> {
+  label: string;
+}
+
+export default function Chip(props: ChipProps) {
+  return (
+    <div
+      {...props}
+      className={clsx(
+        "rounded-full bg-[#FF8577] px-6 py-1",
+        props.className && props.className
+      )}
+    >
+      {props.label}
+    </div>
+  );
+}
